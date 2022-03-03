39179e429bd67d3cf5e35c905698968303fb8329
mmitiushkin <mmitiushkin@gmail.com>
2022-03-03 21:56:50 +0300

    added models updated templates

--- MyPortfolio/mainapp/views.py
+++ MyPortfolio/mainapp/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
+from .models import Project
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'mainapp/index.html', context)
 
 
 def works(request):
     return render(request, 'mainapp/works.html')
 
 
-def work_details(request):
-    return render(request, 'mainapp/work_detail.html')
+def work_details(request, pk):
+    project = Project.objects.get(pk=pk)
+    techs = project.technologies.split(',')
+    funcs = project.functions.split(',')
 
+    has_overview = True
+    if not project.overview: has_overview = False
 
+    has_funcs = True
+    if funcs == ['']: has_funcs = False
 
+    context = {'project': project,
+               'techs': techs,
+               'funcs': funcs,
+               'has_funcs': has_funcs,
+               'has_overview': has_overview}
+    return render(request, 'mainapp/work_detail.html', context)
